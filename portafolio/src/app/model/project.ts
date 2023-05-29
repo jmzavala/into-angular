115d5e0e641614e5a95d48be16defb7bb0d0a053
JuanManuel Zavala <jmzavala.olv@gmail.com>
2023-05-29 18:36:21 -0400

    Refactor: se cambia el nombre work por project

--- portafolio/src/app/model/project.ts
+++ portafolio/src/app/model/project.ts
@@ -2,6 +2,7 @@ export interface Project {
   id?: number;
   name: string;
   description: string;
+  color?:string;
   date: Date;
   tags:string[];
   git?: string;
--- portafolio/src/app/pages/home/home.component.ts
+++ portafolio/src/app/pages/home/home.component.ts
@@ -9,12 +9,54 @@ import {Project} from "../../model/project";
 export class HomeComponent {
 
   projects: Project[] = [];
+  //Colors:
 
+  //    background-color: #ffe0cb;
+  //    background-color: #eaffd0;
+  //    background-color: #d0eaff;
+  //    background-color: #eaffd0;
+  //    background-color: #e2dbf8;
+  //    background-color: #fbe0f2;
+  //    background-color: #eaeff3;
   constructor() {
     this.projects.push({
+      id:1,
       name: "Portafolio",
       description: "Mi portafolio personal",
       date: new Date(),
+      color:"#eaffd0",
+      tags: ["Angular", "Bootstrap", "HTML", "CSS", "TypeScript"],
+      git: "",});
+    this.projects.push({
+      id:2,
+      name: "Proyecto 2",
+      description: "Descripción del proyecto 2",
+      date: new Date(),
+      color:"#e2dbf8",
+      tags: ["Angular", "Bootstrap", "HTML", "CSS", "TypeScript"],
+      git: "",});
+    this.projects.push({
+      id:3,
+      name: "Proyecto 3",
+      description: "Descripción del proyecto 3",
+      date: new Date(),
+      color:"#fbe0f2",
+      tags: ["Angular", "Bootstrap", "HTML", "CSS", "TypeScript"],
+      git: "",});
+    this.projects.push({
+      id:4,
+      name: "Proyecto 4",
+      description: "Descripción del proyecto 4",
+      date: new Date(),
+      color:"#eaeff3",
+      tags: ["Angular", "Bootstrap", "HTML", "CSS", "TypeScript"],
+      git: "",});
+    this.projects.push({
+      id:5,
+      name: "Proyecto 5",
+      description: "Descripción del proyecto 5",
+      date: new Date(),
+      color:"#ffe0cb",
       tags: ["Angular", "Bootstrap", "HTML", "CSS", "TypeScript"],
       git: "",});
 
