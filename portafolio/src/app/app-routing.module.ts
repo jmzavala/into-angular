10feec6a83297b7da1dbf36b38af09cce7c838cc
JuanManuel Zavala <jmzavala.olv@gmail.com>
2023-05-29 11:41:44 -0400

    Step 2_1 componentes creados y routers

--- portafolio/src/app/app-routing.module.ts
+++ portafolio/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {ProyectoComponent} from "./pages/proyecto/proyecto.component";
+import {PerfilComponent} from "./pages/perfil/perfil.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+},{
+  path: 'proyecto',
+  component: ProyectoComponent
+},{
+  path:'perfil',
+  component: PerfilComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProyectoComponent } from './pages/proyecto/proyecto.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProyectoComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
